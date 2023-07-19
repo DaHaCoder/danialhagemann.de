62d2968813e91fc958040d414c5b1e66caed6af3
DaHaCoder <dhnerd@protonmail.com>
2023-07-19 21:48:50 +0200

    Update 'assets/js/copyCode.js'

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -2,38 +2,17 @@ function stripLinenums(code) {
     const emptyLineOrOnlySpaces = /^\s*$/;
     const lineWithOnlyNumbers = /^[0-9]+$/;
 
+    const skipLine = line => emptyLineOrOnlySpaces.test(line) || lineWithOnlyNumbers.test(line);
+
     const lines = code.split(/\r?\n/);
 
-    let newCode = [];
-
     let i = 0;
-    while (emptyLineOrOnlySpaces.test(lines[i]) || lineWithOnlyNumbers.test(lines[i])) {
+    while (i < lines.length && skipLine(lines[i])) {
         ++i;
     }
 
-    for (; i < lines.length; ++i) {
-        newCode.push(lines[i]);
-    }
-
-    return newCode.join("\n"); // FIXME: This will probably not work on Windows
+    return lines.slice(i).join("\n"); // FIXME: This will probably not work on Windows
 }
-
-
-// function stripLinenums(code) {
-//     const lineWithOnlyNumbers = /^[0-9]+$/;
-
-//     let newCode = [];
-
-//     for (const line of code.split(/\r?\n/)) {
-//         if (line === "" || lineWithOnlyNumbers.test(line)) {
-//             continue;
-//         }
-
-//         newCode.push(line);
-//     }
-
-//     return newCode.join("\n"); // FIXME: This will probably not work on Windows
-// }
 
 const codeBlocks = document.querySelectorAll('figure > pre > code');
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
