bd128e9afb05edb495c1ae64737a8f7a2160cf78
DaHaCoder <dhnerd@protonmail.com>
2023-07-19 21:26:59 +0200

    Update 'assets/js/copyCode.js'

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,16 +1,51 @@
+function stripLinenums(code) {
+    const lineWithOnlyNumbers = /^[0-9]+$/;
+
+    let newCode = [];
+
+    for (const line of code.split(/\r?\n/)) {
+        if (line === "" || lineWithOnlyNumbers.test(line)) {
+            continue;
+        }
+
+        newCode.push(line);
+    }
+
+    return newCode.join("\n"); // FIXME: This will probably not work on Windows
+}
+
 const codeBlocks = document.querySelectorAll('figure > pre > code');
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+
 copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerText;
+    const code = stripLinenums(codeBlocks[index].innerText);
 
-  copyCodeButton.addEventListener('click', () => {
-    window.navigator.clipboard.writeText(code);
-    copyCodeButton.classList.remove('fa-copy');
-    copyCodeButton.classList.add('fa-check');
+    copyCodeButton.addEventListener('click', () => {
+        window.navigator.clipboard.writeText(code);
+        copyCodeButton.classList.remove('fa-copy');
+        copyCodeButton.classList.add('fa-check');
 
-    setTimeout(() => {
-      copyCodeButton.classList.remove('fa-check');
-      copyCodeButton.classList.add('fa-copy');
-    }, 2000);
-  });
+        setTimeout(() => {
+            copyCodeButton.classList.remove('fa-check');
+            copyCodeButton.classList.add('fa-copy');
+        }, 2000);
+    });
 });
+
+
+// const codeBlocks = document.querySelectorAll('figure > pre > code');
+// const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+// copyCodeButtons.forEach((copyCodeButton, index) => {
+//   const code = codeBlocks[index].innerText;
+
+//   copyCodeButton.addEventListener('click', () => {
+//     window.navigator.clipboard.writeText(code);
+//     copyCodeButton.classList.remove('fa-copy');
+//     copyCodeButton.classList.add('fa-check');
+
+//     setTimeout(() => {
+//       copyCodeButton.classList.remove('fa-check');
+//       copyCodeButton.classList.add('fa-copy');
+//     }, 2000);
+//   });
+// });
